f5ace33c14a8f16f48e5648cf5c124e23e233673
wangyang <wangyang.guo@intel.com>
2017-09-17 12:14:23 +0800

    + dtree: best_feature_split

--- dtree.py
+++ dtree.py
@@ -5,6 +5,7 @@ import math
 def entroy(dataset):
     '''
     get Shannon Entroy of dataset
+    higher Entroy means more chaos
 
     params:
         dataset: NxM array data, the last element of each row is label
@@ -21,3 +22,56 @@ def entroy(dataset):
         prob = float(count)/total
         ent -= prob*math.log(prob, 2)
     return ent
+
+
+def split_dataset(dataset, index, value):
+    '''
+    split dataset
+    filter out data vec with given index and value
+
+    params:
+        dataset: dataset to be processed
+        index: index of vec to check
+        value: target value to match
+
+    return
+    '''
+    ret_data = []
+    for vec in dataset:
+        if vec[index] == value:
+            # pop up value at index
+            # then we can reduce complexity by 1 axes
+            reduced_vec = vec[:index]
+            reduced_vec.extend(vec[index+1:])
+            ret_data.append(reduced_vec)
+    return ret_data
+
+
+def best_feature_split(dataset):
+    '''
+    choose best feature to split
+
+    params:
+        dataset: input dataset, the last member of vec is label
+
+    return: the index of best feature for split in vec
+    '''
+    feature_num = len(dataset[0]) - 1  # exclude label
+    base_entroy = entroy(dataset)
+    best_index = -1
+    best_info_gain = 0
+    for i in range(feature_num):
+        # all possible values at index i
+        values = set([vec[i] for vec in dataset])
+        ientroy = 0  # total entroy of this split
+        for v in values:
+            sub_set = split_dataset(dataset, i, v)
+            # the length of sub_set is how many time value=v appears
+            # so probility is len(sub_set)/ (total samples)
+            prob = len(sub_set)/float(len(dataset))
+            ientroy += entroy(sub_set)
+        info_gain = base_entroy - ientroy
+        if (info_gain > best_info_gain):
+            best_info_gain = info_gain
+            best_index = i
+    return best_index
--- tests/test_dtree.py
+++ tests/test_dtree.py
@@ -3,15 +3,25 @@ from .base import TestBase
 import dtree
 
 
+def create_dataset():
+    dataset = [
+        [1, 1, 'yes'],
+        [1, 1, 'yes'],
+        [1, 0, 'no'],
+        [0, 1, 'no'],
+        [0, 1, 'no']
+    ]
+    return dataset
+
+
 class DtreeTest(TestBase):
     def test_entroy(self):
-        dataset = [
-            [1, 1, 'yes'],
-            [1, 1, 'yes'],
-            [1, 0, 'no'],
-            [0, 1, 'no'],
-            [0, 1, 'no']
-        ]
+        dataset = create_dataset()
         entroy = dtree.entroy(dataset)
         logging.info(entroy)
         self.assertTrue(0.97 < entroy < 0.98)
+
+    def test_best_feature_split(self):
+        dataset = create_dataset()
+        index = dtree.best_feature_split(dataset)
+        self.assertEqual(index, 0)
